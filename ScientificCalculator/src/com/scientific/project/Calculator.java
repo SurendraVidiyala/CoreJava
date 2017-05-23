cd7ff251c2e5968bc3c1e50c0cfd639b1f638639
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-05-22 21:02:33 -0500

    Scientific Calculator program includes default and the converter

---
+++ ScientificCalculator/src/com/scientific/project/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.scientific.project;
+
+/**
+ *
+ * @author SUNNY
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Default d=new Default();
+        d.setVisible(true);
+        d.setSize(512,512);
+        
+    }
+}
